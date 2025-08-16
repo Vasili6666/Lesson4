a87f7c8ed9e547c985cc437d660bcd1b165d6ec5
Vasili Pl <w2e3r4t51982@gmail.com>
2025-08-16 16:47:42 +0200

    Comments are deleted

--- src/test/java/CheckExistingCodeForJUnite5Test.java
+++ src/test/java/CheckExistingCodeForJUnite5Test.java
@@ -8,23 +8,11 @@ public class CheckExistingCodeForJUnite5Test {
     @Test
     void fillFormTest() {
         open("https://github.com/");
-
-        // Открыть сайт и кликнуть на поле поиска
         $("span[data-target='qbsearch-input.inputButtonText']").shouldBe(visible).click();
-
-        // Вести слово selenide
         $("#query-builder-test").setValue("selenide").pressEnter();
-
-        // Найти и нажать selenide/selenide
         $("span.search-match").shouldHave(text("selenide/selenide")).click();
-
-        // Поиск и нажатие Wiki
         $("#wiki-tab").click();
-        // Поиск и нажатие Soft assertions
         $(byText("Soft assertions")).click();
-
-        //Проверка наличия в "wiki-body" текста "Using JUnit5"
-        //$("#wiki-body").shouldHave(text("ExtendWith"));
         String expectedCode = """
     @ExtendWith({SoftAssertsExtension.class})
     class Tests {
@@ -38,9 +26,6 @@ public class CheckExistingCodeForJUnite5Test {
     }""";
 
         $("#wiki-body").shouldHave(text(expectedCode));
-
-        //sleep(5000);
-
         Selenide.closeWebDriver();
     }
 }
